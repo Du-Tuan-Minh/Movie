b713525188f6103f8342784c00cf1da1b09ab800
dutuanminh <dutuanminh2812202@gmail.com>
2025-02-28 15:17:59 +0700

    Initial Commit

---
+++ Movies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movies
+//
+//  Created by DuTuanMinh on 28/2/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
